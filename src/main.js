945ec48612259c452587ecd42464e61462447a3b
zanwonzzzz <etnicastro9@gmail.com>
2025-10-28 11:43:26 -0600

    se me hace que ya

--- src/main.js
+++ src/main.js
@@ -112,3 +112,15 @@ async function initApp() {
 
 // Inicializar la aplicación
 initApp()
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('SW registrado:', registration)
+      })
+      .catch(error => {
+        console.log('SW registro falló:', error)
+      })
+  })
+}
