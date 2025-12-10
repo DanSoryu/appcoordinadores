815fe876b67efdf87f49664ae704b04553af7375
DanSoryu <dd592526@gmail.com>
2025-12-10 11:23:09 -0600

    solicitud refacciones 2

--- src/components/SolicitudRefacciones.vue
+++ src/components/SolicitudRefacciones.vue
@@ -1,16 +1,23 @@
 <template>
-  <div>
-    <SolicitudRefaccionesTable />
+  <div class="min-h-screen bg-gray-50">
+    <AppHeader :user="user" v-model:activeTab="activeTab" />
+
+    <!-- Main Content -->
+    <main class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <div v-if="activeTab === 'solicitudes'">
+        <SolicitudRefaccionesTable />
+      </div>
+    </main>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+import AppHeader from './global/AppHeader.vue'
 import SolicitudRefaccionesTable from './solicitud_refacciones/SolicitudRefaccionesTable.vue'
 
-export default {
-  name: 'SolicitudRefacciones',
-  components: {
-    SolicitudRefaccionesTable
-  }
-}
+const authStore = useAuthStore()
+const user = computed(() => authStore.user)
+const activeTab = ref('solicitudes')
 </script>
